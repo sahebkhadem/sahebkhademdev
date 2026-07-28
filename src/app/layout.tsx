febda2014713acca87384b6f495e6cf92ef81750
Saheb Khadem <saheb1379@gmai.com>
2026-07-28 23:57:26 +0330

    Initialize shadcn/ui and, replace favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,16 @@
 import "./globals.css";
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const geistHeading = Geist({ subsets: ["latin"], variable: "--font-heading" });
+
+const jetbrainsMono = JetBrains_Mono({
+	subsets: ["latin"],
+	variable: "--font-mono"
+});
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -26,7 +35,15 @@ export default function RootLayout({
 	return (
 		<html
 			lang="en"
-			className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+			className={cn(
+				"h-full",
+				"antialiased",
+				geistSans.variable,
+				geistMono.variable,
+				"font-mono",
+				jetbrainsMono.variable,
+				geistHeading.variable
+			)}
 		>
 			<body className="flex min-h-full flex-col">{children}</body>
 		</html>
