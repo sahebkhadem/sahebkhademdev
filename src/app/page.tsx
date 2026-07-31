b1e3f24350c09ea67bbea5cf2b55fd6211e8ffa8
Saheb Khadem <saheb1379@gmai.com>
2026-07-31 18:45:19 +0330

    Refactor home page layout for mobile responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import TechStackTile from "@/components/tiles/tech-stack/TechStackTile";
 
 export default function Home() {
 	return (
-		<main className="grid h-screen w-full grid-cols-1 gap-4 p-4 md:grid-cols-12 md:grid-rows-12">
+		<main className="flex min-h-screen w-full flex-col gap-4 p-4 md:grid md:h-screen md:grid-cols-12 md:grid-rows-12">
 			<IdentityTile className="md:col-span-7 md:row-span-4" />
 			<StatusTile className="md:col-span-5 md:row-span-4" />
 			<TechStackTile className="md:col-span-4 md:row-span-8" />
